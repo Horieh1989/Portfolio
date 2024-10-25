78f3489b1d421dbb0c34adb423175fc91c992c32
Your Name <horieh.ghiasidalalian@iths.se>
2024-10-25 04:15:17 -0700

    jsfile

--- styles/app.js
+++ styles/app.js
@@ -1,3 +1,23 @@
 const Sections= document.querySelectorAll(".section");
 const SectBtns= document.querySelectorAll(".controls");
 const SectBtn= document.querySelectorAll(".contrl");
+const allSections= document.querySelectorAll(".main-content");
+
+
+
+
+function PageTransitions(){
+    for (let i=0; i< SectBtn.length; i++){
+        SectBtn[i].addEventListener("click",function(){
+    
+            let currentBtn= document.querySelector(".active-btn");
+            currentBtn[0].className=currentBtn[0].className.replace(".active-btn","");
+            this.className +="active-btmn"
+            })
+        
+        
+        
+        
+        }
+}
+PageTransitions();
